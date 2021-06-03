57e8153a324736385967ca8f091933701fd7e375
chenk <ke.chen@xiaoxitech.com>
2021-06-03 18:14:08 +0800

    fix

--- tencloud/src/main/java/com/chenk/tencentcloud/controller/UploadFileController.java
+++ tencloud/src/main/java/com/chenk/tencentcloud/controller/UploadFileController.java
@@ -1,6 +1,7 @@
 package com.chenk.tencentcloud.controller;
 
 import com.chenk.tencentcloud.util.TencentCOSUploadFileUtil;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,14 +11,16 @@ import org.springframework.web.multipart.MultipartFile;
  * @author: chenke
  * @since: 2021/6/3
  */
+@Slf4j
 @RestController
 public class UploadFileController {
     @PostMapping("/upload")
-    public String upload(@RequestParam("file") MultipartFile file){
+    public String upload(@RequestParam("file") MultipartFile file) {
         if (null == file) {
             return "文件为空";
         }
         String filePath = TencentCOSUploadFileUtil.uploadfile(file);
-        return "上传成功，访问地址为:"+filePath;
+        log.info("上传成功，访问地址为:" + filePath);
+        return "上传成功，访问地址为:" + filePath;
     }
 }
